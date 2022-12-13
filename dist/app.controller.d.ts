63d8b26dba2d125bb2c0ee149c5a1a6225401576
italomoraess <italomoraes.sth@gmail.com>
2022-12-13 16:38:30 -0300

    Setup prisma and create table notification

--- dist/app.controller.d.ts
+++ dist/app.controller.d.ts
@@ -1,6 +1,8 @@
-import { AppService } from './app.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
 export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
+    private readonly prisma;
+    constructor(prisma: PrismaService);
+    list(): import(".prisma/client").PrismaPromise<import(".prisma/client").Notification[]>;
+    create(body: CreateNotificationBody): Promise<void>;
 }
